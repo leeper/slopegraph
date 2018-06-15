a4985cf687a8989db5219b5f41312bcd5ff40295
Chuck Powell <ibecav@gmail.com>
2018-06-15 15:33:40 -0400

    adding cancer2

--- R/data.R
+++ R/data.R
@@ -8,6 +8,18 @@
 #' @keywords datasets
 "cancer"
 
+#' @rdname cancer2
+#' @title Estimates of relative survival rates, by cancer site (other)
+#' @aliases cancer2
+#' @description Cancer survival rates for 5, 10, 15, and 20 years for twenty-four cancer sites.
+#' @format
+#' A data frame containing long-formatted data from \code{\link{cancer}}, yielding 96 observations for variables \dQuote{Type}, \dQuote{Year}, and \dQuote{Survival} (surival rate).
+#' 
+#' @source Edward Tufte. \emph{Beautiful Evidence}. Graphics Press, 174-176.
+#' @seealso For data in \dQuote{wide} format, see \code{\link{cancer2}}
+#' @keywords datasets
+"cancer2"
+
 #' @title Current receipts of government as a percentage of Gross Domestic Product, 1970 and 1979
 #' @aliases gdp
 #' @description Current receipts of fifteen national governments as a percentage of gross domestic product
